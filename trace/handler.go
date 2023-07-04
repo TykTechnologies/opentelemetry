1e8f920dcf69d03c7ae640f5180c134423dc614c
tbuchaillot <tombuchaillot89@gmail.com>
2023-07-04 18:41:48 +0200

    adding code comments

--- trace/handler.go
+++ trace/handler.go
@@ -6,6 +6,11 @@ import (
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
+// NewHTTPHandler wraps the provided http.Handler with one that starts a span
+// and injects the span context into the outbound request headers.
+// You need to initialize the TracerProvider first since it utilizes the underlying
+// TracerProvider and propagators.
+// It also utilizes a spanNameFormatter to format the span name r.Method + " " + r.URL.Path.
 func NewHTTPHandler(name string, handler http.Handler, tp Provider) http.Handler {
 	opts := []otelhttp.Option{
 		otelhttp.WithSpanNameFormatter(httpSpanNameFormatter),
@@ -20,6 +25,8 @@ var httpSpanNameFormatter = func(operation string, r *http.Request) string {
 	return r.Method + " " + r.URL.Path
 }
 
+// NewHTTPTransport wraps the provided http.RoundTripper with one that
+// starts a span and injects the span context into the outbound request headers.
 func NewHTTPTransport(base http.RoundTripper) http.RoundTripper {
 	return otelhttp.NewTransport(base)
 }
