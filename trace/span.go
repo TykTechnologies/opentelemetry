1f499723faf18b4268d68a92d92a782063dc5b27
Matias <83959431+mativm02@users.noreply.github.com>
2023-07-11 09:45:40 -0300

    adding examples to comments

--- trace/span.go
+++ trace/span.go
@@ -9,6 +9,12 @@ import (
 type Span trace.Span
 
 // SpanFromContext returns the span attached to the given context.
+// If the context does not have a span attached to it, a no-op span will be returned.
+// Example:
+//
+//	ctx := context.Background()
+//	span := trace.SpanFromContext(ctx)
+//	defer span.End()
 func SpanFromContext(ctx context.Context) Span {
 	return trace.SpanFromContext(ctx)
 }
@@ -18,6 +24,11 @@ func SpanFromContext(ctx context.Context) Span {
 // If the context does not have a span attached to it, the new span will be a root span.
 // The tracer name is used to identify the tracer to be used to create the span.
 // If the tracer name is not provided, the default 'tyk' tracer name will be used.
+// Example:
+//
+//	ctx := context.Background()
+//	ctx, span := trace.NewSpanFromContext(ctx, "my-tracer", "my-span")
+//	defer span.End()
 func NewSpanFromContext(ctx context.Context, tracerName, spanName string) (context.Context, Span) {
 	if tracerName == "" {
 		tracerName = "tyk"
