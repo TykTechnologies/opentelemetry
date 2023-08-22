bb11dedfa9beb3d7fa38dd049c54e410f93c5f63
Matias <83959431+mativm02@users.noreply.github.com>
2023-08-22 10:34:46 -0300

    adding request_content_length and response_content_length to parent span

--- trace/handler.go
+++ trace/handler.go
@@ -7,23 +7,42 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
-// NewHTTPHandler wraps the provided http.Handler with one that starts a span
-// and injects the span context into the outbound request headers.
-// You need to initialize the TracerProvider first since it utilizes the underlying
-// TracerProvider and propagators.
-// It also utilizes a spanNameFormatter to format the span name r.Method + " " + r.URL.Path.
+type responseWriterWrapper struct {
+	http.ResponseWriter
+	contentLength int
+	onWrite       func(int) // callback for when Write is called
+}
+
+func (rw *responseWriterWrapper) Write(b []byte) (int, error) {
+	n, err := rw.ResponseWriter.Write(b)
+	rw.contentLength += n
+	rw.onWrite(rw.contentLength)
+	return n, err
+}
+
 func NewHTTPHandler(name string, handler http.Handler, tp Provider, attr ...Attribute) http.Handler {
 	opts := []otelhttp.Option{
 		otelhttp.WithSpanNameFormatter(httpSpanNameFormatter),
 	}
-	if len(attr) > 0 {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		attr = append(attr, NewAttribute("http.request_content_length", r.ContentLength))
+
 		opts = append(opts, otelhttp.WithSpanOptions(
 			trace.WithAttributes(attr...),
 		))
-	}
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		otelhttp.NewHandler(handler, name, opts...).ServeHTTP(w, r)
+		rwWrapper := &responseWriterWrapper{
+			ResponseWriter: w,
+			onWrite: func(length int) {
+				span := trace.SpanFromContext(r.Context())
+				if span != nil {
+					span.SetAttributes(NewAttribute("http.response_content_length", int64(length)))
+				}
+			},
+		}
+
+		otelhttp.NewHandler(handler, name, opts...).ServeHTTP(rwWrapper, r)
 	})
 }
 
