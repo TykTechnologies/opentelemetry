da9e9c7b85355595b6ced7fccf4836b73f892a98
sredny buitrago <sredny.buitrago@gmail.com>
2023-08-22 15:21:26 -0400

    reformat of config options documentation

--- config/config.go
+++ config/config.go
@@ -1,81 +1,81 @@
 package config
 
 type OpenTelemetry struct {
-	// enabled is a flag that can be used to enable or disable the trace exporter.
+	// Enabled is a flag that can be used to enable or disable the trace exporter.
 	Enabled bool `json:"enabled"`
-	// exporter is the type of the exporter to sending data in OTLP protocol.
+	// Exporter is the type of the exporter to sending data in OTLP protocol.
 	// This should be set to the same type of the OpenTelemetry collector.
 	// Valid values are "grpc", or "http".
-	// Defaults to "grpc"
+	// Defaults to "grpc".
 	Exporter string `json:"exporter"`
-	// endpoint is the OpenTelemetry collector endpoint to connect to.
-	// Defaults to "localhost:4317"
+	// Endpoint is the OpenTelemetry collector endpoint to connect to.
+	// Defaults to "localhost:4317".
 	Endpoint string `json:"endpoint"`
-	// headers is a map of headers that will be sent with HTTP requests to the collector.
+	// Headers is a map of headers that will be sent with HTTP requests to the collector.
 	Headers map[string]string `json:"headers"`
-	// connection_timeout is the timeout for establishing a connection to the collector.
+	// Connection_timeout is the timeout for establishing a connection to the collector.
 	// Defaults to 1 second.
 	ConnectionTimeout int `json:"connection_timeout"`
-	// resource_name is the name of the resource that will be used to identify the resource.
-	// Defaults to "tyk"
+	// Resource_name is the name of the resource that will be used to identify the resource.
+	// Defaults to "tyk".
 	ResourceName string `json:"resource_name"`
-	// span_processor_type is the type of the span processor to use.
+	// Span_processor_type is the type of the span processor to use.
 	// Valid values are "simple" or "batch".
-	// Defaults to "batch"
+	// Defaults to "batch".
 	SpanProcessorType string `json:"span_processor_type"`
-	// context_propagation is the type of the context propagator to use.
+	// Context_propagation is the type of the context propagator to use.
 	// Valid values are:
 	// - "tracecontext": tracecontext is a propagator that supports the W3C
 	// Trace Context format (https://www.w3.org/TR/trace-context/).
 	// - "b3": b3 is a propagator serializes SpanContext to/from B3 multi Headers format.
-	// Defaults to "tracecontext"
+	// Defaults to "tracecontext".
 	ContextPropagation string `json:"context_propagation"`
-	// tls is the TLS configuration for the exporter.
+	// Tls is the TLS configuration for the exporter.
 	TLS TLS `json:"tls"`
-	// Sampling defines the configurations to use in the sampler
+	// Sampling defines the configurations to use in the sampler.
 	Sampling Sampling `json:"sampling"`
 }
 
 type TLS struct {
-	// enable is a flag that can be used to enable TLS.
+	// Enable is a flag that can be used to enable TLS.
 	// Defaults to false (disabled).
 	Enable bool `json:"enable"`
-	// insecure_skip_verify is a flag that can be used to skip TLS verification if TLS is enabled.
+	// Insecure_skip_verify is a flag that can be used to skip TLS verification if TLS is enabled.
 	// Defaults to false.
 	InsecureSkipVerify bool `json:"insecure_skip_verify"`
-	// ca_file is the path to the CA file.
+	// Ca_file is the path to the CA file.
 	CAFile string `json:"ca_file"`
-	// cert_file is the path to the cert file.
+	// Cert_file is the path to the cert file.
 	CertFile string `json:"cert_file"`
-	// key_file is the path to the key file.
+	// Key_file is the path to the key file.
 	KeyFile string `json:"key_file"`
-	// max_version is the maximum TLS version that is supported.
-	// options: ["1.0", "1.1", "1.2", "1.3"]
-	// Defaults to "1.3"
+	// Max_version is the maximum TLS version that is supported.
+	// Options: ["1.0", "1.1", "1.2", "1.3"].
+	// Defaults to "1.3".
 	MaxVersion string `json:"max_version"`
-	// min_version is the minimum TLS version that is supported.
-	// options: ["1.0", "1.1", "1.2", "1.3"]
-	// Defaults to "1.2"
+	// Min_version is the minimum TLS version that is supported.
+	// Options: ["1.0", "1.1", "1.2", "1.3"].
+	// Defaults to "1.2".
 	MinVersion string `json:"min_version"`
 }
 
 type Sampling struct {
-	// type refers to the policy used by OpenTelemetry to determine
+	// Type refers to the policy used by OpenTelemetry to determine
 	// whether a particular trace should be sampled or not. It's determined at the
 	// start of a trace and the decision is propagated down the trace. Valid Values are:
-	// AlwaysOn, AlwaysOff and TraceIDRatioBased. It defaults to AlwaysOn
+	// AlwaysOn, AlwaysOff and TraceIDRatioBased. It defaults to AlwaysOn.
 	Type string `json:"type"`
-	// sampling_rate is a parameter for the TraceIDRatioBased sampler type. It represents
-	// the percentage of traces to be sampled. The value should be a float between 0.0 (0%) and 1.0 (100%).
-	// If the sampling rate is 0.5, the sampler will aim to sample approximately 50% of traces.
-	// it defaults to 0.5
+	// Sampling_rate is a parameter for the TraceIDRatioBased sampler type and represents the percentage
+	// of traces to be sampled. The value should fall between 0.0 (0%) and 1.0 (100%). For instance, if
+	// the sampling rate is set to 0.5, the sampler will aim to sample approximately 50% of the traces.
+	// By default, it's set to 0.5.
 	Rate float64 `json:"rate"`
-	// parent_based is a rule that makes sure that if we decide to record data
-	// for a particular operation, we'll also record data for all the work that operation
-	// causes (its "child spans"). This helps keep the whole story of a transaction together.
-	// You usually use ParentBased with TraceIDRatioBased, because with AlwaysOn or AlwaysOff,
-	// you're either recording everything or nothing, so there are no decisions to respect.
-	// It defaults to false
+	// Parent_based is a rule that ensures that if we decide to record data for a particular operation,
+	// we'll also record data for all the subsequent work that operation causes (its "child spans").
+	// This approach helps in keeping the entire story of a transaction together. Typically, ParentBased
+	// is used in conjunction with TraceIDRatioBased. Using it with AlwaysOn or AlwaysOff might not be as
+	// effective since, in those cases, you're either recording everything or nothing, and there are no
+	// intermediary decisions to consider. The default value for this option is false.
 	ParentBased bool `json:"parent_based"`
 }
 
